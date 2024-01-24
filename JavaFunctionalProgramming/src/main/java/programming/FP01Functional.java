bc37135f0ae04b84e51e467d47f0b4d0fab9bf17
Caio Bianchi <caiohbs23@gmail.com>
2024-01-24 10:08:50 -0300

    added "JavaFunctionalProgramming" project

---
+++ JavaFunctionalProgramming/src/main/java/programming/FP01Functional.java
@@ -0,0 +1,28 @@
+package programming;
+
+import java.util.List;
+
+public class FP01Functional {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(12, 9, 4, 5, 22, 0, 9, 39);
+
+        System.out.println("---Printing all numbers---");
+        printAllNumbersInListFunctional(List.of(12, 9, 4, 5, 22, 0, 9, 39));
+
+        System.out.println("\n---Printing even numbers---");
+        printEvenNumbersInListFunctional(numbers);
+    }
+
+    private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+        // Sends each of the numbers in the list to be printed.
+        numbers.stream()
+                .forEach(System.out::println); // Method reference.
+    }
+
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+        // Sends each of the numbers in the list to be printed.
+        numbers.stream()
+                .filter(number -> number % 2 == 0) // Lambda expression.
+                .forEach(System.out::println); // Method reference.
+    }
+}
---
+++ JavaFunctionalProgramming/src/main/java/programming/FP01Structured.java
@@ -0,0 +1,29 @@
+package programming;
+
+import java.util.List;
+
+public class FP01Structured {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(12, 9, 4, 5, 22, 0, 9, 39);
+
+        System.out.println("---Printing all numbers---");
+        printAllNumbersInListStructured(numbers);
+
+        System.out.println("\n---Printing even numbers---");
+        printEvenNumbersInListStructured(numbers);
+    }
+
+    private static void printAllNumbersInListStructured(List<Integer> numbers) {
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+    private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        }
+    }
+}
