3b534932d789a985c7cfa3134967425dbaced472
Caio Bianchi <caiohbs23@gmail.com>
2024-01-25 10:42:28 -0300

    finished "d1" project

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/d1/LazyInitializationLauncher.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/d1/LazyInitializationLauncher.java
@@ -3,15 +3,35 @@ package com.in28minutes.learnspringframework.examples.d1;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+@Component
+class ClassA {
+    public ClassA() {
+    }
+}
+
+@Component
+@Lazy
+class ClassB {
+    public ClassB() {
+        System.out.println("\n---Dummy initialization logic for class B---\n".toUpperCase());
+    }
+
+    public void doSomething() {
+        System.out.println("CLASS_B_METHOD: \"Doing Something...\"");
+    }
+}
+
 
 @Configuration
 @ComponentScan
 public class LazyInitializationLauncher {
     public static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(LazyInitializationLauncher.class)) {
-            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+            System.out.println("SYSTEM: Initialization of context is completed");
+            context.getBean(ClassB.class).doSomething();
         }
     }
 }
