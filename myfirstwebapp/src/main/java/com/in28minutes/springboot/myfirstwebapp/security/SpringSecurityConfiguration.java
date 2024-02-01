71f29adb16d3634d7b7be1abea8419198ab4fdf5
Caio Bianchi <caiohbs23@gmail.com>
2024-02-01 10:45:24 -0300

    refactored code to create new users

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -14,15 +14,23 @@ import java.util.function.Function;
 public class SpringSecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
-        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+        UserDetails userDetails1 = createNewUser("caiohbs", "123456");
+        UserDetails userDetails2 = createNewUser("dummy", "123456");
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
+        Function<String, String> passwordEncoder
+                = input -> passwordEncoder().encode(input);
+
         UserDetails userDetails = User.builder()
                 .passwordEncoder(passwordEncoder)
-                .username("caiohbs")
-                .password("123456")
+                .username(username)
+                .password(password)
                 .roles("USER", "ADMIN")
                 .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
+        return userDetails;
     }
 
     @Bean
