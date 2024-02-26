1e89564cc7740a9e1684323ff314f47966832423
Caio Bianchi <caiohbs23@gmail.com>
2024-02-26 16:48:16 -0300

    added new components and css code change

--- fullstack-spring-react-todo-app/frontend/src/App.jsx
+++ fullstack-spring-react-todo-app/frontend/src/App.jsx
@@ -4,14 +4,21 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
 import LoginComponent from "./components/todo/LoginComponent.jsx";
-import WelcomeComponent from "./components/todo/WelcomeComponent.jsx";
 import ErrorComponent from "./components/todo/ErrorComponent.jsx";
+import FooterComponent from "./components/todo/FooterComponent.jsx";
+import HeaderComponent from "./components/todo/HeaderComponent.jsx";
+import LogoutComponent from "./components/todo/LogoutComponent.jsx";
+import WelcomeComponent from "./components/todo/WelcomeComponent.jsx";
 import ListTodosComponent from "./components/todo/ListTodosComponent.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/login",
     element: <LoginComponent />,
+  },
+  {
+    path: "/logout",
+    element: <LogoutComponent />,
   },
   {
     path: "/welcome/:username",
@@ -30,7 +37,9 @@ const router = createBrowserRouter([
 export default function App() {
   return (
     <div className="App">
+      <HeaderComponent />
       <RouterProvider router={router} />
+      <FooterComponent />
     </div>
   );
 }
---
+++ fullstack-spring-react-todo-app/frontend/src/components/todo/LogoutComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function LogoutComponent() {
+  return (
+    <div className="LogoutComponent">
+      <h1>
+        You are now logged out! Please login again if you want to keep using out
+        app.
+      </h1>
+      <div>We hope to see you back soon :)</div>
+    </div>
+  );
+}
