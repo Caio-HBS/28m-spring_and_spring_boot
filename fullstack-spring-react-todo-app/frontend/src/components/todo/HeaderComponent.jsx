c3de5ddf130045215fcb5e2f9df687c87b42e003
Caio Bianchi <caiohbs23@gmail.com>
2024-02-27 11:48:35 -0300

    rehauled header component

--- fullstack-spring-react-todo-app/frontend/src/components/todo/HeaderComponent.jsx
+++ fullstack-spring-react-todo-app/frontend/src/components/todo/HeaderComponent.jsx
@@ -2,8 +2,45 @@ import React from "react";
 
 export default function HeaderComponent() {
   return (
-    <div className="header">
-      Header <hr />
-    </div>
+    <header className="border-bottom border-light border-5 mb-5 p-2">
+      <div className="container">
+        <div className="row">
+          <nav className="navbar navbar-expand-lg">
+            <a
+              className="navbar-brand ms-2 fs-2 fw-bold text-black"
+              href="https://github.com/Caio-HBS"
+            >
+              Caio-HBS
+            </a>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav">
+                <li className="nav-item fs-5">
+                  <a className="nav-link" href="/welcome/Caio-HBS">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item fs-5">
+                  <a className="nav-link" href="/todos">
+                    Todos
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <ul className="navbar-nav">
+              <li className="nav-item fs-5">
+                <a className="nav-link" href="/">
+                  Login
+                </a>
+              </li>
+              <li className="nav-item fs-5">
+                <a className="nav-link" href="/logout">
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
 }
