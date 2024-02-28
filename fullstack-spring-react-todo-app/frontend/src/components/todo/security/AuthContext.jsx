552563007ec90e919b711bcd6a07145a2f4497e2
Caio Bianchi <caiohbs23@gmail.com>
2024-02-28 16:05:53 -0300

    final changes on first context for first phase

--- fullstack-spring-react-todo-app/frontend/src/components/todo/security/AuthContext.jsx
+++ fullstack-spring-react-todo-app/frontend/src/components/todo/security/AuthContext.jsx
@@ -1,14 +1,10 @@
 import { createContext, useContext, useState } from "react";
-import { Outlet } from "react-router-dom";
-
-import FooterComponent from "../FooterComponent.jsx";
-import HeaderComponent from "../HeaderComponent.jsx";
 
 export const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
-export default function AuthProvider() {
+export default function AuthProvider({ children }) {
   const [isAuthenticated, setAuthenticated] = useState(false);
 
   function login() {
@@ -20,9 +16,7 @@ export default function AuthProvider() {
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
-      <HeaderComponent />
-      <Outlet />
-      <FooterComponent />
+      {children}
     </AuthContext.Provider>
   );
 }
