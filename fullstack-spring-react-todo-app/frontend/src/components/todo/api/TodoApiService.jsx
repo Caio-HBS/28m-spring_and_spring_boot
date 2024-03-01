32bb12c11aaa9fe4c8f5bdcb2c98a3ff68d31e56
Caio Bianchi <caiohbs23@gmail.com>
2024-03-01 15:51:52 -0300

    added new call to api service

--- fullstack-spring-react-todo-app/frontend/src/components/todo/api/TodoApiService.jsx
+++ fullstack-spring-react-todo-app/frontend/src/components/todo/api/TodoApiService.jsx
@@ -3,6 +3,8 @@ import { apiClient } from "./ApiClient";
 export const retrieveAllTodosForUsernameApi = (username) =>
   apiClient.get(`/users/${username}/todos`);
 
-  export const deleteSingleTodoApi = (username, id) =>
+export const deleteSingleTodoApi = (username, id) =>
   apiClient.delete(`/users/${username}/todos/${id}`);
 
+export const retrieveSingleTodoApi = (username, id) =>
+  apiClient.get(`/users/${username}/todos/${id}`);
