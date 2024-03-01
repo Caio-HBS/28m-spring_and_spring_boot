6ac121b1cb129033dab868777fd080eada74c8a8
Caio Bianchi <caiohbs23@gmail.com>
2024-03-01 15:08:46 -0300

    updates on todo service and temporary changes on pom.xml

--- fullstack-spring-react-todo-app/backend/src/main/java/com/caiohbs/restful/todo/TodoService.java
+++ fullstack-spring-react-todo-app/backend/src/main/java/com/caiohbs/restful/todo/TodoService.java
@@ -14,12 +14,12 @@ public class TodoService {
     private static int todosCount = 0;
 
     static {
-        todos.add(new Todo(++todosCount, "in28minutes","Get AWS Certified",
+        todos.add(new Todo(++todosCount, "Caio-HBS","Get AWS Certified",
                 LocalDate.now().plusYears(10), false ));
-        todos.add(new Todo(++todosCount, "in28minutes","Learn DevOps",
+        todos.add(new Todo(++todosCount, "Caio-HBS","Learn DevOps",
                 LocalDate.now().plusYears(11), false ));
-        todos.add(new Todo(++todosCount, "in28minutes","Learn Full Stack Development",
-                LocalDate.now().plusYears(12), false ));
+        todos.add(new Todo(++todosCount, "Caio-HBS","Learn Full Stack Development",
+                LocalDate.now().plusYears(12), true ));
     }
 
     public List<Todo> findByUsername(String username){
