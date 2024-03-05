ccf714367580967c2d8a857367cc98450d94a6ea
Caio Bianchi <caiohbs23@gmail.com>
2024-03-05 18:40:36 -0300

    added jwt-related objects

---
+++ fullstack-spring-react-todo-app/backend/src/main/java/com/caiohbs/restful/jwt/JwtTokenRequest.java
@@ -0,0 +1,4 @@
+package com.caiohbs.restful.jwt;
+
+public record JwtTokenRequest(String username, String password) {
+}
---
+++ fullstack-spring-react-todo-app/backend/src/main/java/com/caiohbs/restful/jwt/JwtTokenResponse.java
@@ -0,0 +1,6 @@
+package com.caiohbs.restful.jwt;
+
+public record JwtTokenResponse(String token) {}
+
+
+
