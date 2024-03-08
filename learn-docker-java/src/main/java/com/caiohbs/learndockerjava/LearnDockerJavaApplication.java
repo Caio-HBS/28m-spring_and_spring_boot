728babb30f0842c0413ea8c8b08607fab90dcda8
Caio Bianchi <caiohbs23@gmail.com>
2024-03-08 10:18:03 -0300

    added new project

---
+++ learn-docker-java/src/main/java/com/caiohbs/learndockerjava/LearnDockerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.caiohbs.learndockerjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDockerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDockerJavaApplication.class, args);
+	}
+
+}
---
+++ learn-docker-java/src/test/java/com/caiohbs/learndockerjava/LearnDockerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiohbs.learndockerjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDockerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
