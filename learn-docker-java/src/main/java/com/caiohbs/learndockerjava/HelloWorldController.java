412ac4984451cb0c5b9ac94aec0763de4eb15f02
Caio Bianchi <caiohbs23@gmail.com>
2024-03-08 10:36:28 -0300

    added new basic controller

---
+++ learn-docker-java/src/main/java/com/caiohbs/learndockerjava/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.caiohbs.learndockerjava;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path="/")
+    public String helloWorld() {
+        return """
+                 { message: Hello World Java v1 }
+                """;
+    }
+
+}
