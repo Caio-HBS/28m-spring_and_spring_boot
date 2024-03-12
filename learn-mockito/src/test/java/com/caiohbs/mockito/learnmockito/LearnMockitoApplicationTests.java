adfac40dabd3686d263763a168d721c7269b72f2
Caio Bianchi <caiohbs23@gmail.com>
2024-03-12 10:15:20 -0300

    added "learn-mockito" project

---
+++ learn-mockito/src/test/java/com/caiohbs/mockito/learnmockito/LearnMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiohbs.mockito.learnmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
