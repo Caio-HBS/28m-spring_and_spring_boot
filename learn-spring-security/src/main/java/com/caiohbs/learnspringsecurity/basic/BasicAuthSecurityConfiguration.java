9c186ad3a25ffb1e24ee20dbab64cc1f187df758
Caio Bianchi <caiohbs23@gmail.com>
2024-03-13 11:21:32 -0300

    added new way to save user details

--- learn-spring-security/src/main/java/com/caiohbs/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/caiohbs/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -27,6 +31,21 @@ public class BasicAuthSecurityConfiguration {
         );
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("Caio-HBS")
+                .password("{noop}123")
+                .roles(String.valueOf(UserRoles.USER))
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("{noop}123")
+                .roles(String.valueOf(UserRoles.ADMIN))
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
---
+++ learn-spring-security/src/main/java/com/caiohbs/learnspringsecurity/basic/UserRoles.java
@@ -0,0 +1,6 @@
+package com.caiohbs.learnspringsecurity.basic;
+
+public enum UserRoles {
+    ADMIN,
+    USER
+}
